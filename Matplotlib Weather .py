09763b4593e6b9a57ee0e6cbade5072812d7d667
Ajinkya Arun Narke <narke.ajinkya@gmail.com>
2018-12-11 11:49:00 +0530

    Add files via upload
    Weather Report

---
+++ Matplotlib Weather .py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+import matplotlib.pyplot as pl
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[16]:
+
+
+x=[1,2,3,4,5,6,7]
+y=[51,53,49,48,47,51,45]
+
+
+# In[27]:
+
+
+pl.xlabel('Day')
+pl.ylabel('Temp')
+pl.plot(x,y,color='green',linewidth=4,linestyle='dotted')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
